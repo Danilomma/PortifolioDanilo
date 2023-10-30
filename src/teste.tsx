ba48d4d1dc66e018e3340c45dadcf5d700722060
Danilomma <nilodani4@gmail.com>
2023-10-30 13:40:06 -0300

    configurando o ambiente

---
+++ src/teste.tsx
@@ -0,0 +1,19 @@
+import { type } from 'os'
+import styled from 'styled-components'
+
+type BotaoProps = {
+  principal: boolean
+}
+const Botao = styled.button<BotaoProps>`
+  background-color: ${(props) => (props.principal ? 'green' : 'blue')};
+`
+
+function Teste() {
+  return (
+    <>
+      <Botao principal>Enviar</Botao>
+      <Botao principal={false}>Cancelar</Botao>
+    </>
+  )
+}
+export default Teste
